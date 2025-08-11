7143fb2c08f2084e03711e6f28edc5ede903ae96
pawankalyan <pawankalyankarri22@gmail.com>
2025-08-12 00:03:50 +0530

    tech navbar setup ready

---
+++ src/components/techNavbar/TechNavbarEmpty.jsx
@@ -0,0 +1,11 @@
+import { Outlet } from "react-router-dom"
+
+const TechNavbarEmpty = () => {
+    return(
+        <div>
+            <Outlet/>
+        </div>
+    )
+}
+
+export default TechNavbarEmpty

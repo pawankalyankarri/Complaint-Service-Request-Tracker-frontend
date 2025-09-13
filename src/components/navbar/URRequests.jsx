b433edd7882f457fa0fed0f31da023a60c153d41
pawankalyan <pawankalyankarri22@gmail.com>
2025-09-13 19:21:10 +0530

    user details saved using sessionStorage

--- src/components/navbar/URRequests.jsx
+++ src/components/navbar/URRequests.jsx
@@ -4,18 +4,27 @@ import { useEffect, useState } from "react";
 const URRequests = ()=>{
     let [reqs,setReqs] = useState([])
     let [areqs,setAreqs] = useState([])
+    let [userDetails,setUserDetails] = useState({})
     useEffect(()=>{
         axios.get('http://127.0.0.1:8000/login/requests').then(res=>{
             setReqs(res.data)
             console.log(res.data)
         }).catch(err=>console.log(err))
+        
     },[])
     useEffect(()=>{
         axios.get('http://127.0.0.1:8000/login/accreq').then(res=>{
             setAreqs(res.data)
             console.log(res.data)
         }).catch(err=>console.log(err))
+        
     },[])
+    
+    useEffect(()=>{
+            setUserDetails(JSON.parse(sessionStorage.getItem('userDetails')))
+            console.log(userDetails)
+    },[])
+    
     
     
     return(
@@ -28,11 +37,22 @@ const URRequests = ()=>{
                     }
                 })
             })} */}
-            {reqs
-      .filter(obj => areqs.some(areq => obj.req_id === areq.areq_unumber))
+            {/* {reqs
+      .filter(obj => areqs.some(areq => obj.req_user === areq.areq_unumber))
       .map(obj => (
         <h1 key={obj.req_id}>{obj.req_brief}</h1>
-      ))}
+      ))} */}
+        {
+            reqs.map(obj=>{
+                if(obj.req_user === userDetails.user_id){
+                    return(
+                        <div key={obj.req_id}>
+                            <h3>{obj.req_brief}</h3>
+                        </div>
+                    )
+                }
+            })
+        }
         </div>
     )
 }
--- src/components/navbar/UserLogin.jsx
+++ src/components/navbar/UserLogin.jsx
@@ -1,69 +1,74 @@
-import axios from "axios"
-import { useRef, useState } from "react"
-import { Link, useNavigate } from "react-router-dom"
+import axios from "axios";
+import { useRef, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+const UserLogin = () => {
+  let navigate = useNavigate();
+  let unameRef = useRef();
+  let pswRef = useRef();
+  let uinfo = false;
+  function handleData(e) {
+    e.preventDefault();
+    let user_uname = unameRef.current.value;
+    let user_psw = pswRef.current.value;
+    axios
+      .get("http://127.0.0.1:8000/login/userlogin/")
+      .then((res) => {
+        let flag = res.data.some(
+          (obj) => obj.user_uname === user_uname && obj.user_psw === user_psw
+        );
+
+        if (flag) {
+          // console.log('both are matched')
+          navigate("/");
+          let u_details = res.data.find(
+            (obj) => obj.user_uname === user_uname && obj.user_psw === user_psw
+          );
+
+          sessionStorage.setItem("userDetails", JSON.stringify(u_details));
 
 
-const UserLogin = () =>{
-    let navigate = useNavigate()
-    let unameRef = useRef()
-    let pswRef = useRef()
-    let uinfo = false
-    let [user_d,setUser_d] = useState({})
-    function handleData(e){
-        e.preventDefault()
-        let user_uname = unameRef.current.value
-        let user_psw = pswRef.current.value
-        axios.get("http://127.0.0.1:8000/login/userlogin/").then((res)=>{            
-            let flag = res.data.some((obj)=>obj.user_uname === user_uname && obj.user_psw === user_psw)
-            console.log(res.data)
-            setUser_d(res.data[0])
-            if (flag){
-                // console.log('both are matched')
-                navigate('/')
-                console.log(user_d)
-            }
-            else
-                // console.log('not matched')
-                //navigate('/userlogin')
+        }
+        // console.log('not matched')
+        //navigate('/userlogin')
+        else unameRef.current.value = "";
+        unameRef.current.placeholder = "Wrong UserName";
+        unameRef.current.style.borderColor = "red";
+        pswRef.current.value = "";
+        pswRef.current.placeholder = "Wrong Password";
+        pswRef.current.style.borderColor = "red";
+        uinfo = true;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 
-                unameRef.current.value = ''
-                unameRef.current.placeholder='Wrong UserName'
-                unameRef.current.style.borderColor = 'red'
-                pswRef.current.value = ''
-                pswRef.current.placeholder = 'Wrong Password'
-                pswRef.current.style.borderColor = 'red'
-                uinfo = true
-
-        }).catch((err)=>{
-            console.log(err)
-        })
-
-    }
-
-
-    return (
-        <div className="d-flex justify-content-center">
-            
-            <form action="" className="card p-4 w-25 shadow" onSubmit={handleData}>
-                <div>
-                    <label htmlFor="" className="form-label">User Name</label>
-                    <input type="text" className="form-control" ref={unameRef} />
-                </div>
-                <div>
-                    <label htmlFor="" className="form-label">Password</label>
-                    <input type="text" className="form-control" ref={pswRef}/>
-                </div>
-                <div className="d-flex justify-content-center align-items-center p-3">
-                    <input type="submit" className="btn btn-success" />&nbsp;&nbsp;
-                    <Link to='/userreg'>Register Here</Link>
-                </div>
-                <div className="d-flex justify-content-center p-1">
-                    
-                </div>
-            </form>
-            {uinfo?<h4 className="text-danger">Wrong Details</h4>:''}
+  return (
+    <div className="d-flex justify-content-center">
+      <form action="" className="card p-4 w-25 shadow" onSubmit={handleData}>
+        <div>
+          <label htmlFor="" className="form-label">
+            User Name
+          </label>
+          <input type="text" className="form-control" ref={unameRef} />
         </div>
-    )
-}
+        <div>
+          <label htmlFor="" className="form-label">
+            Password
+          </label>
+          <input type="text" className="form-control" ref={pswRef} />
+        </div>
+        <div className="d-flex justify-content-center align-items-center p-3">
+          <input type="submit" className="btn btn-success" />
+          &nbsp;&nbsp;
+          <Link to="/userreg">Register Here</Link>
+        </div>
+        <div className="d-flex justify-content-center p-1"></div>
+      </form>
+      {uinfo ? <h4 className="text-danger">Wrong Details</h4> : ""}
+    </div>
+  );
+};
 
-export default UserLogin
+export default UserLogin;
